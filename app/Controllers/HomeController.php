7409fa735e87989707b698a33c161ef2b6b2546a
BriceHarper <58884670+BriceHarper@users.noreply.github.com>
2025-11-21 23:43:16 +0100

    Initial commit

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+
+class HomeController
+{
+    
+    public function __construct()
+    {
+        
+
+    }
+
+    public function index()
+    {
+        include __DIR__ . "/../../resources/views/HomePage.php";
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+    - Commande à exécuter pour lancer le serveur : php -S localhost:8001
+*/
+// Require composer autoloader
+require __DIR__ . '/vendor/autoload.php';
+use Bramus\Router\Router;
+// Create Router instance
+$router = new Router();
+
+// Config
+$router->setNamespace("App\Controllers");
+$router->set404(function(){
+    echo "erreur";
+});
+
+// Routes
+$router->get('/', "HomeController@index");
+
+
+$router->get('/chapter/{id}', "ChapterController@show");
+// Run it!
+$router->run();
