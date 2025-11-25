d3e62cf278e3d409b78cd6593cdaa3168f9668df
HEYBERGER <nathanael.heyberger@etu.unicaen.fr>
2025-11-25 09:53:02 +0100

    add: registration request

--- app/Controllers/SignUpController.php
+++ app/Controllers/SignUpController.php
@@ -1,19 +1,41 @@
 <?php
 namespace App\Controllers;
+use Src\Core\DatabaseController;
+use Throwable;
+
 class SignUpController{
+
     public function __construct()
     {
         
 
     }
     
-    public function index()
+    public function register()
     {
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            include __DIR__ . "/../../resources/views/HomePage.php";
-        } else{
-
+        $alreadyUsedEmail=0;
+        $username = $_POST["username"];
+        $password = $_POST["password_1"];
+        $email = $_POST["email"];
+    
+        $req_verif_1 = DatabaseController::getInstance()->prepare("SELECT count(*) FROM DUNGEON_USER WHERE email='$email'");
+        $req_verif_1->execute();
+        if($req_verif_1>0){
+            if($_POST["password_1"]==$_POST["password_2"]){
+                $req = DatabaseController::getInstance()->prepare("INSERT INTO (username, password, email) VALUES (:username, :password, :email)");
+                $req->bindParam(':username',$username);
+                $req->bindParam(':password',$password);
+                $req->bindParam(':email',$email);
+                $req->execute();
+                include __DIR__ . "/../../resources/views/HomePage.php";
+            }
+        } else {
             include __DIR__ . "/../../resources/views/SignUpPage.php";
         }
+    }
+
+    public function index()
+    {
+        include __DIR__ . "/../../resources/views/SignUpPage.php";
     }
 }
--- resources/views/SignUpPage.php
+++ resources/views/SignUpPage.php
@@ -9,7 +9,10 @@
     <h1>SignUp</h1>
     <form action="" method="post">
         <label>Username : </label><input id="username" type="text">
-        <label>Password : </label><input id="password" type="text">
+        <label>Email address : </label><input id="email" type="text">
+        <label>Password : </label><input id="password_1" type="password">
+        <label>Confirm Password : </label><input id="password_2" type="password">
+        
         <input type="submit" value="Send">
     </form>
 </body>
