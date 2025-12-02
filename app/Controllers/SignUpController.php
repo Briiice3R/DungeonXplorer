f99390ebb0c53911e287a0e1717067ec91eed26a
DarkNatha008 <nathanael.heyberger@gmail.com>
2025-12-02 09:56:42 +0100

    add: systeme d'inscription fonctionnel + sécurisation de l'intégrité des données

--- app/Controllers/SignUpController.php
+++ app/Controllers/SignUpController.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controllers;
 use App\Core\Database;
+use App\Utils\SessionInitializer;
 use Throwable;
+
+
 
 class SignUpController{
 
@@ -14,19 +17,44 @@ class SignUpController{
     public function register()
     {
         $alreadyUsedEmail=0;
-        $username = $_POST["username"];
+        $alreadyUsedUsername=0;
+        $username = trim($_POST["username"]);
         $password = $_POST["password_1"];
-        $email = $_POST["email"];
+        $email = trim($_POST["email"]);
+        $genre = $_POST["genre"];
+
+        $regex_email = "/^(?=.{1,254}$)(?=^[^@]{1,64}@)(?:\"(?:\\\\.|[^\"\\\\])*\"|[A-Za-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*)@(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)(?:\\.(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?))*$/";
     
         $req_verif_1 = Database::getInstance()->prepare("SELECT count(*) FROM user_dungeon WHERE email='$email'");
         $req_verif_1->execute();
+        $req_verif_2 = Database::getInstance()->prepare("SELECT count(*) FROM user_dungeon WHERE username='$username'");
+        $req_verif_2->execute();
         if($req_verif_1->fetchColumn()<1){
+            $alreadyUsedEmail=0;
+            $_SESSION["alreadyUsedEmail"]="False";
+        } else {
+            $alreadyUsedEmail=1;
+            $_SESSION["alreadyUsedEmail"]="True";
+        }
+        if($req_verif_2->fetchColumn()<1){
+            $alreadyUsedUsername=0;
+            $_SESSION["alreadyUsedUsername"]="False";
+        } else {
+            $alreadyUsedUsername=1;
+            $_SESSION["alreadyUsedUsername"]="True";
+        }
+
+
+        if($alreadyUsedEmail!=1 && $alreadyUsedUsername!=1 && $username!="" && $password!="" && $email!="" && ($genre=="H"||$genre=="F"||$genre=="A") && preg_match($regex_email, $email)){
             if($_POST["password_1"]==$_POST["password_2"]){
-                $req = Database::getInstance()->prepare("INSERT INTO user_dungeon (username, password, email, admin) VALUES (:username, :password, :email, 0)");
+                $req = Database::getInstance()->prepare("INSERT INTO user_dungeon (username, password, email, gender, admin) VALUES (:username, :password, :email, :gender, 0)");
                 $req->bindParam(':username',$username);
                 $req->bindParam(':password',$password);
                 $req->bindParam(':email',$email);
+                $req->bindParam(':gender',$genre);
                 $req->execute();
+                $_SESSION["alreadyUsedEmail"]="False";
+                $_SESSION["alreadyUsedUsername"]="False";
                 include __DIR__ . "/../../resources/views/HomePage.php";
             }
         } else {
--- resources/views/SignUpPage.php
+++ resources/views/SignUpPage.php
@@ -1,3 +1,14 @@
+<?php
+use App\Utils\SessionInitializer;
+if (!isset($_SESSION["alreadyUsedEmail"])) {
+    $_SESSION["alreadyUsedEmail"] = "False";
+}
+if (!isset($_SESSION["alreadyUsedUsername"])) {
+    $_SESSION["alreadyUsedUsername"] = "False";
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,9 +20,20 @@
     <h1>SignUp</h1>
     <form action="/signup" method="POST">
         <label>Username : </label><input id="username" name="username" type="text">
+        <?php 
+            if($_SESSION["alreadyUsedUsername"]=="True"){
+                echo "<label>Nom d'utilisateur déjà utilisé</label>";
+            }
+        ?>
         <label>Email address : </label><input id="email" name="email" type="text">
+        <?php 
+            if($_SESSION["alreadyUsedEmail"]=="True"){
+                echo "<label>Adresse email déjà utilisé</label>";
+            }
+        ?>
         <label>Password : </label><input id="password_1" name="password_1" type="password">
         <label>Confirm Password : </label><input id="password_2" name="password_2" type="password">
+        <label><input type="radio" name="genre" value="H">Homme</label><label><input type="radio" name="genre" value="F">Femme</label><label><input type="radio" name="genre" value="A">Autre</label>
         
         <input type="submit" value="Send">
     </form>
